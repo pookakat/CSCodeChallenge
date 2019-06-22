f13e3acaefa7021fe0e6801693119b92e83a862d
Katie Humburg <pookakat@gmail.com>
2019-06-21 22:35:22 -0400

    round entered number and any tip to nearest penny then return the entire number, including a hundreths place zero

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,9 +2,11 @@ $('#bill').keydown(function(e) {
     var key = e.which;
     if (key == 13) {
         $('#bill').submit();
-        var bill=$('#bill').val(); 
+        var bill=$('#bill').val();
+        bill = Math.round(bill * 100)/100; 
+        bill = bill.toFixed(2);
         alert("Submitted! The total is " + bill);
-        calcTip(bill);
+        calcTip(parseFloat(bill));
         }
     });
 
@@ -16,15 +18,21 @@ $('#bill').keydown(function(e) {
 
     function okTip(amount){
         var tipTotal=amount * 0.15;
+        var tipTotal = Math.round(tipTotal * 100)/100;
+        var tipTotal = tipTotal.toFixed(2);
         alert("Customery minimum tip is 15%. Please leave " + tipTotal + " to satisfy that amount.");
     };
 
     function goodTip(amount){
         var tipTotal = amount * 0.2;
+        var tipTotal = Math.round(tipTotal * 100)/100;
+        var tipTotal = tipTotal.toFixed(2);
         alert("A good tip is usually 20%. Please leave " + tipTotal + " to satisfy that amount.");
     };
 
     function greatTip(amount){
         var tipTotal = amount * 0.25;
+        var tipTotal = Math.round(tipTotal * 100)/100;
+        var tipTotal = tipTotal.toFixed(2);
         alert("A great tip is usually 25% or more. Please leave at least " + tipTotal + " to satisfy that amount.");
     };

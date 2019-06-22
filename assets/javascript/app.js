38c03ae15f1fe7ac41471d67870c2f4904f16333
Katie Humburg <pookakat@gmail.com>
2019-06-21 22:10:50 -0400

    create an event when the press of the enter key enters the bill amount

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+$('#bill').keydown(function(e) {
+    var key = e.which;
+    if (key == 13) {
+    $('#bill').submit();
+    var bill=$('#bill').val(); 
+    alert("Submitted! The total is " + bill);
+    }
+    
+    });

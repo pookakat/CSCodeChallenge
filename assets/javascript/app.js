8fef1a8bf5565056f637c7591ef3583b7205fbab
Katie Humburg <pookakat@gmail.com>
2019-06-21 22:17:07 -0400

    move the tip calculation call to happen when enter is pushed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,20 @@
 $('#bill').keydown(function(e) {
     var key = e.which;
     if (key == 13) {
-    $('#bill').submit();
-    var bill=$('#bill').val(); 
-    alert("Submitted! The total is " + bill);
-    }
-    calcTip(bill);
+        $('#bill').submit();
+        var bill=$('#bill').val(); 
+        alert("Submitted! The total is " + bill);
+        calcTip(bill);
+        }
     });
+
+    function calcTip(amount){
+        okTip(amount);
+        /*goodTip(amount);
+        greatTip(amount);*/
+    };
+
+    function okTip(amount){
+        var tipTotal=amount * 0.12;
+        alert("Customery minimum tip is 12%. Please leave " + tipTotal + " to satisfy that amount.");
+    };
